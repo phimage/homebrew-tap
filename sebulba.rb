5a3ccbd9a9c0d7677375e91ab9d7a2253daad9e7
Eric Marchand <phimage@users.noreply.github.com>
2022-12-03 23:40:57 +0100

    Update sebulba.rb

--- sebulba.rb
+++ sebulba.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Sebulba < Formula
+  desc "Swift command line to remove de-integrate reference to cocoa pods "
   homepage 'https://github.com/phimage/sebulba'
   url 'https://github.com/phimage/sebulba/releases/download/1.0.0/sebulba.zip'
-  head 'https://github.com/phimage/sebulba.git'
-  md5 'bbc0c8565c325b917f126ff5166f3577'
+  sha256 "e9dc8117e2333548ef5869b1f1297d8b6fd6f8c215a7c2a8f3794174a6b52667"
   license "MIT"
 
 
